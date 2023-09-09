adbcc864603bec408eff85500264463b5422d9eb
Josef Schönberger <josef.schoenberger@tum.de>
2023-09-09 19:56:06 +0200

    Retune default parameters

--- include/args.h
+++ include/args.h
@@ -6,7 +6,7 @@ public:
 	std::string cgroup_path = "/sys/fs/cgroup";
 	std::string slice_path = "/user.slice/";
 	float window_seconds = 10.0;
-	unsigned event_thresh = 30;
+	unsigned event_thresh = 50;
 
 	Args(int argc, char** argv);
 };
